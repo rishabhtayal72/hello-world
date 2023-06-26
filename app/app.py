787799cf88a7b3ebc7458fecf5417c36e7548302
Rishabh Tayal <rishabhtayal@Rishabhs-MacBook-Pro.local>
2023-06-26 08:09:57 +0530

    revamp commit

---
+++ app/app.py
@@ -0,0 +1,7 @@
+from time import sleep
+
+# the program will print hello world
+#  every 1 second forever..
+while True:
+    print("Hi, this is Hello World Application!")
+    sleep(1)
--- consumer/consumer.py
+++ consumer/consumer.py
@@ -3,5 +3,5 @@ from time import sleep
 # the program will print hello world
 #  every 1 second forever..
 while True:
-    print("Hello, World")
+    print("Hi, this is Hello World Consumer!")
     sleep(1)
