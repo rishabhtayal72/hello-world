facd14f2aa7fe804fa8232b6bc8596e7bb1c221b
Rishabh Tayal <37596568+rishabhtayal72@users.noreply.github.com>
2023-07-11 15:51:56 +0530

    Create app_new.py

---
+++ app/app_new.py
@@ -0,0 +1,14 @@
+from requests_oauthlib.oauth2_session import OAuth2Session
+
+scope = ['https://www.api.example.com/auth/example.data']
+
+oauth = OAuth2Session(
+    'example_client_id',
+    redirect_uri='https://callback.example.com/uri',
+    scope=scope)
+
+token = oauth.fetch_token(
+        'https://api.example.com/o/oauth2/token',
+        client_secret='example_Password') # Noncompliant
+
+data = oauth.get('https://www.api.example.com/oauth2/v1/exampledata')
