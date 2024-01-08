5871fb269d3b1c60a0a7dcdd88eb821fd8e65195
tayalrishabh96 <135199635+tayalrishabh96@users.noreply.github.com>
2024-01-08 12:58:08 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -5,5 +5,5 @@ os.system('ls -latrh')
 # the program will print hello world
 #  every 2 second forever....
 while True:
-    print("Hi, this is Hello World Application! 23rdNov2023")
+    print("Hi, this is Hello World Application! 8-jan-2024")
     sleep(2)
