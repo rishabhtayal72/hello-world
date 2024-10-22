3a8bc8323df19362cb0a594e38af305026bf590a
Rishabh Tayal <37596568+rishabhtayal72@users.noreply.github.com>
2024-10-22 19:17:32 +0530

    Update consumer.py

--- consumer/consumer.py
+++ consumer/consumer.py
@@ -3,5 +3,5 @@ from time import sleep
 # the program will print hello world
 #  every 1 second forever.
 while True:
-    print("Hi, this is Hello World Consumer! 22nd-Oct-2024")
+    print("Hi, this is Hello World Consumer! 22nd-Oct-2024 ...")
     sleep(1)
